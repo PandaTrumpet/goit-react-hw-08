765298f2157d0396883c54ce7d38154d47bbcc9d
PandaTrumpet <145181488+PandaTrumpet@users.noreply.github.com>
2024-05-17 13:43:55 +0300

    updtae

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authreducer from "./auth/slice";
 import contactReducer from "./contacts/slice";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 export const store = configureStore({
   reducer: {
     contacts: contactReducer,
