be1c1cf238a42f85c2f05dfe82d11f97faa70721
PandaTrumpet <dsavontrumpet@gmail.com>
2024-05-18 21:06:29 +0300

    update

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,7 @@
 import { Suspense, lazy, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import Layout from "../Layout/Layout";
+import RestrictedRoute from "../RestrictedRoute";
 import { useDispatch, useSelector } from "react-redux";
 import { refreshUser } from "../../redux/auth/operations";
 import { selectIsRefreshing } from "../../redux/auth/selectors";
@@ -10,6 +11,9 @@ const LoginPage = lazy(() => import("../../page/LoginPage/LoginPage"));
 const RegistrationPage = lazy(() =>
   import("../../page/RegistrationPage/RegistrationPage")
 );
+{
+  /* <RegistrationPage /> */
+}
 function App() {
   const isRefreshing = useSelector(selectIsRefreshing);
   const dispatch = useDispatch();
@@ -26,8 +30,14 @@ function App() {
             <Routes>
               <Route path="/" element={<HomePage />} />
               <Route path="/contacts" element={<ContactsPage />} />
-              <Route path="/login" element={<LoginPage />} />
-              <Route path="/register" element={<RegistrationPage />} />
+              <Route
+                path="/login"
+                element={<RestrictedRoute component={<LoginPage />} />}
+              />
+              <Route
+                path="/register"
+                element={<RestrictedRoute component={<RegistrationPage />} />}
+              />
             </Routes>
           </Suspense>
         </Layout>
