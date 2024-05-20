26827704d93065682b81eb0cdceffde7ae84cc59
PandaTrumpet <dsavontrumpet@gmail.com>
2024-05-20 20:44:19 +0300

    update

--- src/redux/filters/selectors.js
+++ src/redux/filters/selectors.js
@@ -1,11 +1,29 @@
 import { createSelector } from "@reduxjs/toolkit";
+
 export const selectSearchContact = (state) => state.filter.name;
+
+// export const selectFilteredContacts = createSelector(
+//   [(state) => state.contacts.items, (state) => state.filter.name],
+//   (contacts, contactFilter) => {
+//     return contacts.filter((contact) =>
+//       contact.name.toLowerCase().includes(contactFilter.toLowerCase())
+//     );
+//   }
+// );
 
 export const selectFilteredContacts = createSelector(
   [(state) => state.contacts.items, (state) => state.filter.name],
   (contacts, contactFilter) => {
-    return contacts.filter((contact) =>
-      contact.name.toLowerCase().includes(contactFilter.toLowerCase())
-    );
+    return contacts.filter((contact) => {
+      if (typeof contactFilter === "string" && contactFilter.trim() !== "") {
+        const filter = contactFilter.toLowerCase();
+        return (
+          contact.name.toLowerCase().includes(filter) ||
+          contact.number.toLowerCase().includes(filter)
+        );
+      } else {
+        return true;
+      }
+    });
   }
 );
